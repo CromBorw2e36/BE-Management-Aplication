2cb3c27e16bcc8fa007ea806bf356535d4d16522
Crom Borw <66833938+CromBorw2e36@users.noreply.github.com>
2024-01-13 13:33:26 +0700

    bổ sung signalR

---
+++ SignalR-Server-Quan-Li-APP/Hubs/NotificationSystem.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using quan_li_app.Helpers;
+using quan_li_app.Models;
+using quan_li_app.Models.DataDB;
+using quan_li_app.Models.DataDB.UserData;
+
+namespace SignalR_Server_Quan_Li_APP.Hubs
+{
+    public class NotificationSystem : Hub
+    {
+        private readonly DataContext _dataContext;
+        private readonly TokenHelper _tokenHelper;
+
+        public NotificationSystem(DataContext dataContext, TokenHelper tokenHelper)
+        {
+            this._dataContext = dataContext;
+            this._tokenHelper = tokenHelper;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            string connectionId = Context.ConnectionId;
+            string getToken = _tokenHelper.GetToken(httpContext.Request);
+            string getUser = _tokenHelper.GetUsername(httpContext.Request);
+            TOKEN tokenByUsername = _dataContext.Tokens.FirstOrDefault(x => x.Token == getToken);
+            if (tokenByUsername.connectionSignalID != connectionId)
+            {
+                tokenByUsername.connectionSignalID = connectionId;
+                _dataContext.Tokens.Update(tokenByUsername);
+                _dataContext.SaveChanges();
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public async Task NotifycationSubscribe(UserInfo user, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, "Thông báo");
+        }
+    }
+}
