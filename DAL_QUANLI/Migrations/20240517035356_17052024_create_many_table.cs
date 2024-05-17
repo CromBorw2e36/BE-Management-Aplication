0e96b111de82f0c81986281993ae1f3e1970c0fb
satoshiAgaki123 <162280908+satoshiAgaki123@users.noreply.github.com>
2024-05-17 10:55:11 +0700

    mirgation datacontext

---
+++ DAL_QUANLI/Migrations/20240517035356_17052024_create_many_table.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace quanliapp.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class _17052024_create_many_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "zip_code",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "language",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "active",
+                table: "National",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.CreateTable(
+                name: "CategoryCommon",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    group_id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    name1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    name2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    name3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    number_order = table.Column<int>(type: "int", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    create_date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    create_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryCommon", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UploadFile",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    table_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    create_date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    create_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file_type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file_size = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file_path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    enabled = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UploadFile", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryCommon");
+
+            migrationBuilder.DropTable(
+                name: "UploadFile");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "zip_code",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "language",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "National",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "active",
+                table: "National",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+        }
+    }
+}
