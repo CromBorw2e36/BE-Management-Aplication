4510c4dc471ee799e9c4fda3296e8ca13a385479
satoshiAgaki123 <162280908+satoshiAgaki123@users.noreply.github.com>
2024-06-26 17:17:58 +0700

    update

---
+++ DAL_QUANLI/Migrations/20240626101256_26062024_create_catalog.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace quanliapp.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class _26062024_create_catalog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    level = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    is_delete = table.Column<bool>(type: "bit", nullable: true),
+                    is_active = table.Column<bool>(type: "bit", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    delete_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    dateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    nationality = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ethnicity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    interests = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    maritalStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modifyDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    BHXH = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CCCD = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    codeCompany = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    avatar = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    avatar16 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    avatar32 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    avatar64 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    department_id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    type_employee_id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    position_id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    type_work_id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    is_delete = table.Column<bool>(type: "bit", nullable: true),
+                    create_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    create_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    delete_by = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Position",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    level = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    is_delete = table.Column<bool>(type: "bit", nullable: true),
+                    is_active = table.Column<bool>(type: "bit", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    delete_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Position", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusEmployee",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    level = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    is_delete = table.Column<bool>(type: "bit", nullable: true),
+                    is_active = table.Column<bool>(type: "bit", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    delete_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusEmployee", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeEmployee",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    level = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    is_delete = table.Column<bool>(type: "bit", nullable: true),
+                    is_active = table.Column<bool>(type: "bit", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    delete_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeEmployee", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeWork",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    level = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    is_delete = table.Column<bool>(type: "bit", nullable: true),
+                    is_active = table.Column<bool>(type: "bit", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    update_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    delete_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    company_code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeWork", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Position");
+
+            migrationBuilder.DropTable(
+                name: "StatusEmployee");
+
+            migrationBuilder.DropTable(
+                name: "TypeEmployee");
+
+            migrationBuilder.DropTable(
+                name: "TypeWork");
+        }
+    }
+}
--- DAL_QUANLI/Models/DataDB/User/EmployeeModel.cs
+++ DAL_QUANLI/Models/DataDB/User/EmployeeModel.cs
@@ -1,4 +1,5 @@
 ﻿using DAL_QUANLI.Models.CustomModel;
+using Microsoft.EntityFrameworkCore;
 using quan_li_app.Models.DataDB.UserData;
 using System;
 using System.Collections.Generic;
@@ -8,8 +9,26 @@ using System.Threading.Tasks;
 
 namespace DAL_QUANLI.Models.DataDB.User
 {
-    public class EmployeeModel : UserInfo
+    public class EmployeeModel
     {
+        public string? id { get; set; }
+        public string? name { get; set; }
+        public DateTime? dateOfBirth { get; set; }
+        public string? address { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? gender { get; set; }
+        public string? nationality { get; set; } // quốc tịch
+        public string? ethnicity { get; set; } //  dân tộc
+        public string? interests { get; set; } // sở thích
+        public string? maritalStatus { get; set; } //  tình trạng hôn nhân
+        public DateTime? modifyDate { get; set; } //  tình trạng hôn nhân
+        public string? BHXH { get; set; } // sở thích
+        public string? CCCD { get; set; } // sở thích
+        public string? codeCompany { get; set; } // công ty 
+        public string? avatar { get; set; } // ảnh đại diện
+        public string? avatar16 { get; set; }  // ảnh đại diện 16x16
+        public string? avatar32 { get; set; }  // ảnh đại diện 32 x 32
+        public string? avatar64 { get; set; } // ảnh đại diện 64 x64
         public string? department_id {  get; set; }
         public string? type_employee_id { get; set; }
         public string? position_id { get; set; }
